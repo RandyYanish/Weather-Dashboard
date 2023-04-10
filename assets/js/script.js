8e809b3c178ea55d3f4218ef83a2912743a62089
RandyYanish <rtaylorfargo@gmail.com>
2023-04-09 21:50:00 -0700

    basic outline
    Started the basic outline of the site and styling. Added some initial const in js, styling in css, and outlined TODO's in comments

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Declarations
+const searchButton = document.getElementById('search-button');
+const citySearchInput = document.getElementById('city-search');
+
+// TODO: save inputs in #city-search to localStorage
+// TODO: Generate button in #search-list => <button class="btn btn-secondary w-100 mb-3 p-2">Atlanta</button> from local storage (maximum 10 previous searches)
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// Event Listeners
+searchButton.addEventListener('click', loadWeatherDashboard)
